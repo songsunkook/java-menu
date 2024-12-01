c5f964a37b0bb0a84cc68b7be0e15d1e786d6b7e
songsunkook <songsunkook@gmail.com>
2024-12-01 14:04:54 +0900

    feat: 한 주에 같은 카테고리는 최대 2회까지만 고를 수 있다

---
+++ src/main/java/menu/domain/Recommender.java
@@ -0,0 +1,30 @@
+package menu.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import menu.util.MenuRandomUtil;
+
+public class Recommender {
+
+    private final MenuRandomUtil menuRandomUtil = new MenuRandomUtil();
+
+    Map<Category, Integer> recommendedCategories = new EnumMap<>(Category.class);
+
+    public Category selectCategory() {
+        Category result;
+        do {
+            result = menuRandomUtil.selectCategory();
+        } while (validateSelectedCategoryCount(result));
+        recommendedCategories.put(result, recommendedCategories.getOrDefault(result, 0) + 1);
+        return result;
+    }
+
+    private boolean validateSelectedCategoryCount(Category result) {
+        return recommendedCategories.getOrDefault(result, 0) + 1 > 2;
+    }
+
+    public Menu selectMenu(Category category) {
+        return menuRandomUtil.selectMenu(category);
+    }
+}
--- src/test/java/menu/RecommenderTest.java
+++ src/test/java/menu/RecommenderTest.java
@@ -1,23 +1,39 @@
 package menu;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import menu.domain.Category;
 import menu.domain.Menu;
-import menu.domain.Recommander;
+import menu.domain.Recommender;
 
 class RecommenderTest {
 
     @Test
     void 추천할_카테고리를_무작위로_정한다() {
-        Recommander recommander = new Recommander();
-        Category category = recommander.selectCategory();
+        Recommender recommender = new Recommender();
+        Category category = recommender.selectCategory();
+    }
+
+    @Test
+    void 같은_카테고리는_최대_2회까지만_고를_수_있다() {
+        Recommender recommender = new Recommender();
+        Map<Category, Integer> result = new EnumMap<>(Category.class);
+        for (int i = 0; i < 5; i++) {
+            Category category = recommender.selectCategory();
+            result.put(category, result.getOrDefault(category, 0) + 1);
+        }
+        assertThat(result.values().stream().anyMatch(count -> count > 2)).isFalse();
     }
 
     @Test
     void 각_코치가_먹을_메뉴를_추천한다() {
-        Recommander recommander = new Recommander();
-        Category category = recommander.selectCategory();
-        Menu menu = recommander.selectMenu(category);
+        Recommender recommender = new Recommender();
+        Category category = recommender.selectCategory();
+        Menu menu = recommender.selectMenu(category);
     }
 }
