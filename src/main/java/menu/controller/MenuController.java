de963c614075f07f4eec79667e957e3b6c7f0ff1
songsunkook <songsunkook@gmail.com>
2024-12-01 14:23:20 +0900

    feat: controller 작성

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,24 @@
+package menu.controller;
+
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class MenuController {
+
+    private final MenuService menuService = new MenuService();
+
+    public void run() {
+        OutputView.start();
+
+        OutputView.inputCoachNames();
+        menuService.setCoaches(InputView.coachNames());
+
+        while (menuService.hasNextCoach()) {
+            String coachName = menuService.nextCoach();
+            OutputView.inputHateMenus(coachName);
+            menuService.setHateMenus(coachName, InputView.menuNames());
+        }
+
+        OutputView.result(menuService.result());
+    }
+}
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,7 +6,7 @@ public class OutputView {
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
 
-    public static void inputNames() {
+    public static void inputCoachNames() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
     }
 
